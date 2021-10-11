ff06de249b11bebcee898894df4c0399c45e1c56
pankaj1491 <pankaj.kumar@dell.com>
2021-10-11 19:31:01 +0530

    few more chages

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "../screens/home/Home";
-import Details from "../screens/details/Details";
+import Details  from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
