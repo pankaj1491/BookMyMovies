b94f0976675cb2af9136625b579afc7069d9795b
pankaj1491 <pankaj.kumar@dell.com>
2021-10-20 23:53:14 +0530

    ready to be pushed as final one

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -18,62 +18,56 @@ const Home = (props) => {
   const [filterForm, setfilterForm] = useState({
     fMovie: '',
     fGenres: [],
-    fArtists: [],
-    ReleaseS: { sDate:new Date()} ,
-    ReleaseE: { eDate:new Date()} 
-
-});
-const inputFilterChangedHandler = (e) => {
-  const state = filterForm;
-  state[e.target.name] = e.target.value;
-  setfilterForm({ ...state })
-
-}
-function isEmpty(value){
-  return (value == null || value.length === 0);
-}
-function filterMovies(movie,genres,artists,releases,releasee) {
-
-  if( !isEmpty(movie) && isEmpty(genres) && isEmpty(artists) ){
-    
-  setRelMovies(movies.filter((item)=> (item.title === movie )));
+    fArtists: []
+  });
+  const [ReleaseS, setReleaseS] = useState("");
+  const [ReleaseE, setReleaseE] = useState("");
+  const releaseDateStartHandler = event => {
+    setReleaseS(event.target.value);
   }
-  
-  else if(!isEmpty(movie) && !isEmpty(artists)){
-    let fname=[];
-    let index=0;
-    let flag='';
-    let sName=[];
-    artists.forEach((item) => {let arr = item.split(' ');fname.push(arr[0]);});
-    movies.forEach((item,i) => {if(item.title === movie ){
-           index=i;
-    }})
+  const releaseDateEndHandler = event => {
+    setReleaseE(event.target.value);
+  }
+  const inputFilterChangedHandler = (e) => {
+    const state = filterForm;
+    state[e.target.name] = e.target.value;
+    setfilterForm({ ...state })
 
-    if(index !== 0){
-      movies[index].artists.forEach((item)=> sName.push(item.first_name));
+  }
+
+
+  const relMovieHandler = () => {
+    let queryString = "?status=RELEASED";
+    if (fMovie !== "") {
+      queryString += "&title=" + fMovie;
     }
-    
-    sName.forEach((item) =>{if( fname.includes(item)){flag='YES'}});
-    if(flag === 'YES' && index !== 0){
-      let newArr = [];
-      newArr.push(movies[index]);
-     setRelMovies(newArr);
-    }else{
-      setRelMovies(movies.filter((item) => item.status === 'RELEASED'));
+    if (fGenres.length > 0) {
+      queryString += "&genre=" + encodeURIComponent(fGenres.toString());
     }
-    
-  }
-  else{
-      
-      setRelMovies(movies.filter((item) => item.status === 'RELEASED'));
-  }
-  }
-  
-const relMovieHandler = () => {
+    if (fArtists.length > 0) {
+      queryString += "&artists=" + encodeURIComponent(fArtists.toString());
+    }
+    if (ReleaseS !== "") {
+      queryString += "&start_date=" + ReleaseS
+    }
+    if (ReleaseE !== "") {
+      queryString += "&end_date=" + ReleaseE
+    }
 
-  filterMovies(fMovie,fGenres,fArtists,ReleaseS,ReleaseE);
+    let dataFilter = null;
+    let xhrFilter = new XMLHttpRequest();
+    xhrFilter.onreadystatechange = () => {
+      if (xhrFilter.readyState === 4) {
+        setRelMovies(JSON.parse(xhrFilter.responseText).movies);
+      }
+    };
+    console.log(props.baseUrl + "movies" + queryString)
+    xhrFilter.open("GET", props.baseUrl + "movies" + queryString);
+    xhrFilter.setRequestHeader("Cache-Control", "no-cache");
+    xhrFilter.send(dataFilter);
+  }
 
-}
+
   const theme = useTheme();
   const cardtheme = createTheme({
 
@@ -117,7 +111,7 @@ const relMovieHandler = () => {
   async function loadMovieData() {
 
     try {
-      const rawResponse = await fetch(props.baseUrl+'movies?page=1&limit=10', {
+      const rawResponse = await fetch(props.baseUrl + 'movies?page=1&limit=10', {
         method: 'GET',
         headers: {
           "Accept": "application/json",
@@ -136,7 +130,7 @@ const relMovieHandler = () => {
   async function loadGenreData() {
 
     try {
-      const rawResponse = await fetch(props.baseUrl+'genres', {
+      const rawResponse = await fetch(props.baseUrl + 'genres', {
         method: 'GET',
         headers: {
           "Accept": "application/json",
@@ -156,7 +150,7 @@ const relMovieHandler = () => {
   async function loadArtistData() {
 
     try {
-      const rawResponse = await fetch(props.baseUrl+'artists?page=1&limit=10', {
+      const rawResponse = await fetch(props.baseUrl + 'artists?page=1&limit=10', {
         method: 'GET',
         headers: {
           "Accept": "application/json",
@@ -175,14 +169,14 @@ const relMovieHandler = () => {
   }
   useEffect(() => {
     loadMovieData();
-  },[])
+  }, [])
   useEffect(() => {
     loadGenreData();
   }, [])
   useEffect(() => {
     loadArtistData();
   }, [])
-const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
+  const { fMovie, fGenres, fArtists } = filterForm;
   return (
     <Fragment>
       <div>
@@ -202,16 +196,16 @@ const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
       </div>
       <div className="releasedMoviesnfilter">
         <div className="releasedMovies">
-          <GridList cellHeight={350} cols={4} style={{width:'80%'}} >
+          <GridList cellHeight={350} cols={4} style={{ width: '80%' }} >
             {Relmovies.map((Movie) => (
-              <GridListTile key={Movie.id}  > 
+              <GridListTile key={Movie.id}  >
                 <Link to={`/movie/${Movie.id}`}>
-                <img style={{ cursor: 'pointer' }} src={Movie.poster_url} alt={Movie.title} />
-                <GridListTileBar title={Movie.title}
-                  subtitle={`Release Date:Fri ${new Date(Movie.release_date).toDateString()}`} />
-                  </Link>
+                  <img style={{ cursor: 'pointer' }} src={Movie.poster_url} alt={Movie.title} />
+                  <GridListTileBar title={Movie.title}
+                    subtitle={`Release Date:Fri ${new Date(Movie.release_date).toDateString()}`} />
+                </Link>
               </GridListTile>
-          
+
             )
 
             )}
@@ -226,7 +220,7 @@ const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
                 titleTypographyProps={{ variant: 'h6' }}
                 title="FIND MOVIES BY:" />
 
-              <CardContent style={{marginTop:'0px',paddingTop:'0px'}}>
+              <CardContent style={{ marginTop: '0px', paddingTop: '0px' }}>
                 <FormControl variant="standard">
                   <InputLabel htmlFor="component-simple">Movie Name</InputLabel>
                   <Input id="component-simple" name="fMovie" value={fMovie} onChange={inputFilterChangedHandler} />
@@ -244,7 +238,7 @@ const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
                       const state = filterForm;
                       state['fGenres'] = val;
                       setfilterForm({ ...state });
-                    
+
                     }
                   }}
                   getOptionLabel={(option) => option}
@@ -277,7 +271,7 @@ const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
                       const state = filterForm;
                       state['fArtists'] = val;
                       setfilterForm({ ...state });
-                    
+
                     }
                   }}
                   getOptionLabel={(option) => option}
@@ -295,17 +289,17 @@ const {fMovie,fGenres,fArtists,ReleaseS,ReleaseE} = filterForm;
                   )}
                   style={{ width: 180 }}
                   renderInput={(params) => (
-                    <TextField {...params} variant='standard' label="Artists"  />
+                    <TextField {...params} variant='standard' label="Artists" />
                   )}
                 />
 
                 <FormControl variant="standard">
                   <InputLabel htmlFor="component-simple" shrink>Release Date Start</InputLabel>
-                  <TextField id="component-simple" variant='standard' type="date" sx={{width:180,paddingTop:2}} name="ReleaseS" value={ReleaseS} onChange={inputFilterChangedHandler} />
+                  <TextField id="component-simple" variant='standard' type="date" sx={{ width: 180, paddingTop: 2 }} name="ReleaseS" value={ReleaseS} onChange={releaseDateStartHandler} />
                 </FormControl>
                 <FormControl variant="standard">
                   <InputLabel htmlFor="component-simple" shrink>Release Date End</InputLabel>
-                  <TextField id="component-simple" variant='standard' type="date" sx={{width:180,paddingTop:2}} name="ReleaseE" value={ReleaseE} onChange={inputFilterChangedHandler}/>
+                  <TextField id="component-simple" variant='standard' type="date" sx={{ width: 180, paddingTop: 2 }} name="ReleaseE" value={ReleaseE} onChange={releaseDateEndHandler} />
                 </FormControl>
                 <div>
                   <Button fullWidth className={cardtheme.components.MuiButton.styleOverrides.root} type='submit' variant="contained" onClick={relMovieHandler}>APPLY</Button>
